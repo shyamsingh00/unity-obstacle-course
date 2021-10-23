e2c82c53847eaaa549d668dd99b84eabdde73117
Shyam Singh <shyams1499@gmail.com>
2021-10-22 23:09:06 -0400

    Added virtual camera, walls, and constraints for collisions

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);    
     }
 }
