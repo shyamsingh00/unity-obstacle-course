2c200653d2a062d09dd65338b7072549503a8f4b
Shyam Singh <shyams1499@gmail.com>
2021-11-15 20:21:15 -0500

    Completed a basic obstacle course

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,14 +8,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();    
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue, 0, zValue);    
+        transform.Translate(xValue, 0, zValue);  
     }
 }
